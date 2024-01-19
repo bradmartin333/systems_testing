320adf04e6ae041d01ea875e65dcc57b4e383c9e
Brad Martin <brad.martin@teledyne.com>
2024-01-19 16:40:51 -0500

    working action button

--- python_ui/main.py
+++ python_ui/main.py
@@ -5,16 +5,24 @@ class WindowState:
     wid = 800
     hgt = 450
 
+    mouse_pos = Vector2(-1, -1)
+    left_click_down = False
+    left_click_pressed = False
+
     last_dpi_scale = Vector2(1, 1)
     dpi_scale = Vector2(1, 1)
     max_dpi_scale = 1
 
     text_fields = []
+    button_actions = []
 
     def __init__(self):
         self.update()
 
     def update(self):
+        self.mouse_pos = get_mouse_position()
+        self.left_click_down = is_mouse_button_down(MouseButton.MOUSE_BUTTON_LEFT)
+        self.left_click_pressed = is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT)
         self.dpi_scale = get_window_scale_dpi()
         if self.last_dpi_scale != self.dpi_scale:
             self.last_dpi_scale = self.dpi_scale
@@ -25,21 +33,72 @@ class WindowState:
 
     def draw_all_text(self):
         for t in self.text_fields:
-            draw_text(
-                t.text,
-                int(t.position.x * self.dpi_scale.x),
-                int(t.position.y * self.dpi_scale.y),
-                int(t.size * self.max_dpi_scale),
-                t.tint,
-            )
+            t.draw(self)
+
+    def draw_all_buttons(self):
+        for b in self.button_actions:
+            b.draw(self)
+
+    def draw_all(self):
+        self.draw_all_text()
+        self.draw_all_buttons()
 
 
 class TextField:
-    def __init__(self, text, position, size, tint=BLACK):
+    def __init__(self, text, position, size=20, tint=BLACK):
         self.text = text
         self.position = position
         self.size = size
         self.tint = tint
+
+    def draw(self, ws: WindowState):
+        draw_text(
+            self.text,
+            int(self.position.x * ws.dpi_scale.x),
+            int(self.position.y * ws.dpi_scale.y),
+            int(self.size * ws.max_dpi_scale),
+            self.tint,
+        )
+
+
+class ActionButton:
+    color = BLUE
+    border_color = BLACK
+    mouse_over_color = DARKBLUE
+    mouse_down_color = Color(0, 82, 240, 255)  # Slightly lighter than DARKBLUE
+
+    def __init__(self, text_field: TextField, action):
+        self.tf = text_field
+        self.action = action
+
+    def draw(self, ws: WindowState):
+        p = 10 * ws.max_dpi_scale
+        x = self.tf.position.x * ws.dpi_scale.x
+        y = self.tf.position.y * ws.dpi_scale.y
+        wid = measure_text(self.tf.text, self.tf.size) * ws.dpi_scale.x
+        hgt = self.tf.size * ws.dpi_scale.y
+        border = Rectangle(x - p, y - p, wid + p * 2, hgt + p * 2)
+        active_area = Rectangle(x - p / 2, y - p / 2, wid + p, hgt + p)
+
+        mouse_over = check_collision_point_rec(ws.mouse_pos, active_area)
+        current_color = self.mouse_over_color if mouse_over else self.color
+        current_color = (
+            self.mouse_down_color
+            if ws.left_click_down and mouse_over
+            else current_color
+        )
+
+        draw_rectangle_rec(border, self.border_color)
+        draw_rectangle_rec(active_area, current_color)
+
+        self.tf.draw(ws)
+
+        if mouse_over and ws.left_click_pressed:
+            self.action()
+
+
+def test_function():
+    print("HOLA!!!!")
 
 
 ws = WindowState()
@@ -48,6 +107,9 @@ ws.text_fields = [
     TextField("HELLO MEDIUM", Vector2(10, 50), 40),
     TextField("HELLO LARGE", Vector2(10, 100), 60),
 ]
+ws.button_actions.append(
+    ActionButton(TextField("HELLO MEDIUM", Vector2(10, 50), 40), test_function)
+)
 
 init_window(ws.wid, ws.hgt, "REM Rest")
 
@@ -55,6 +117,6 @@ while not window_should_close():
     ws.update()
     begin_drawing()
     clear_background(DARKGRAY)
-    ws.draw_all_text()
+    ws.draw_all()
     end_drawing()
 close_window()
